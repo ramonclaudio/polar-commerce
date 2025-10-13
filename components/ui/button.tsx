8b4f273e7e4fe96cf4399df1a91655c1e172fe12
Ray <hi@rmncldyo.com>
2025-10-12 21:36:55 -0400

    style(ui): remove extra blank lines for consistency

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,7 +3,6 @@
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
-
 import { cn } from '@/lib/shared/utils';
 
 const buttonVariants = cva(
